c4398c8cce67142b482ada89c08d3063c74213f2
Nathan Rossow <nathanrossow@gmail.com>
2026-07-31 18:28:21 -0500

    Create stower-data-72326.py

---
+++ stower-data-72326.py
@@ -0,0 +1,18 @@
+Rank (from top),Stower,Approx. Stow Rate (boxes/hour)
+1,Stower 1,220
+2,Stower 2,215
+3,Stower 3,210
+4,Stower 4,208
+5,Stower 5,206
+6,Stower 6,204
+7,Stower 7,203
+8,Stower 8,202
+9,Stower 9,201
+10,Stower 10,200.5
+11,Stower 11 (known),200
+12,Stower 12,100
+13,Stower 13,80
+14,Stower 14,70
+15,Stower 15,60
+16,Stower 16,40
+17,Stower 17,30.5
