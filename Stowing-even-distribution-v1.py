bb5c0ff07d13f82e391a226509c3ac9c8b3fb5a4
Nathan Rossow <nathanrossow@gmail.com>
2026-08-01 03:52:13 -0500

    Implement stowing work distribution calculations
    Add calculations for stowing work distribution

---
+++ Stowing-even-distribution-v1.py
@@ -0,0 +1,19 @@
+# 12100/220
+
+# 55 hours of stowing work
+# 12100 ÷ 220 = 55​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+
+# 55/17 
+
+# 55 ÷ 17 = 3.235 hours per stower
+
+# How many ten minute intervals are there in 3.23 hours
+
+# 19.38
+# There are 60 minutes in an hour, so:
+# 3.23 hours × 60 = 193.8 minutes.
+# A 10-minute interval is 10 minutes, so:
+# 193.8 ÷ 10 = 19.38.​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+
+
+
